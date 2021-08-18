ed7da70c4df5003a5c3206b96db5ae5b449b1611
crimii112 <ycl623@naver.com>
2021-08-18 20:18:05 +0900

    commit

--- app/src/main/java/com/example/whyyou/Friend.kt
+++ app/src/main/java/com/example/whyyou/Friend.kt
@@ -2,16 +2,19 @@ package com.example.whyyou
 
 import android.graphics.Insets.add
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.Insets.add
 import androidx.core.view.OneShotPreDrawListener.add
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.friend.*
 import kotlinx.android.synthetic.main.friend.view.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class Friend : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,32 +22,50 @@ class Friend : AppCompatActivity() {
         setContentView(R.layout.friend)
         recView!!.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
-        val datas = mutableListOf<FriendData>()
+        var datas = mutableListOf<FriendData>()
 
         val friendAdapter : FriendAdapter = FriendAdapter(this)
 
+        // 로그아웃 버튼 눌렀을 때 로그인 화면으로 이동
         btn_sign_out.setOnClickListener {
             Firebase.auth.signOut()
             startActivity<LoginActivity>()
         }
 
+        // 친구추가 버튼 눌렀을 때 친구 요청 화면으로 이동
         btnFriendAdd.setOnClickListener {
             startActivity<FriendRequest>()
         }
 
-        val name = Firebase.auth.currentUser?.email
+        val currentUserEmail = Firebase.auth.currentUser?.email
 
-        datas.apply {
-            name?.let { FriendData(it) }?.let { add(it) }
+        val db = Firebase.firestore
+        val docRef = db.collection(currentUserEmail!!).document("Friend List")
+        docRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w("TAG", "Listen failed.", e)
+                return@addSnapshotListener
+            }
 
+            if (snapshot != null && snapshot.exists()) {
+                val d = Log.d("TAG", "data: ${snapshot.data}")
+                toast(snapshot.data?.get("friend_name").toString())
+
+                val friendList = snapshot.data?.get("friend_name") as ArrayList<*>
+                val listSize = friendList.size
+
+                for (i in 0 until listSize) {
+                    datas.apply {
+                        add(FriendData(friendList[i] as String))
+                    }
+                }
+
+                friendAdapter.replaceList(datas)
+                recView.adapter = friendAdapter
+
+            } else {
+                Log.d("TAG", "data: null")
+            }
         }
-
-        friendAdapter.replaceList(datas)
-        recView.adapter = friendAdapter
-
-//        btnFriendAdd.setOnClickListener {
-//
-//        }
-
     }
 }
--- app/src/main/java/com/example/whyyou/FriendRequest.kt
+++ app/src/main/java/com/example/whyyou/FriendRequest.kt
@@ -4,24 +4,26 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.friend_listview.*
 import kotlinx.android.synthetic.main.friend_request.*
 import org.jetbrains.anko.toast
 
-@Suppress("NAME_SHADOWING")
+@Suppress("NAME_SHADOWING", "UNCHECKED_CAST")
 class FriendRequest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.friend_request)
 
+
         // 요청 버튼 눌렀을 때
         btn_request.setOnClickListener {
 
             val firestore = Firebase.firestore
-            val currentUser = Firebase.auth.uid   // 현재 사용자 uid (계정 구별하는 키)
+            val currentUserEmail = Firebase.auth.currentUser!!.email   // 현재 사용자 uid (계정 구별하는 키)
             val friendId = search_id.text.toString()   // 검색할 친구 아이디
-            val friendList = arrayListOf<String>()    // 친구 이름 저장할 배열
 
             // DB에서 아이디 검색해서 친구 이름 가져오는거
             firestore.collection("users")
@@ -33,43 +35,50 @@ class FriendRequest : AppCompatActivity() {
                     for(name in it!!.documents) {
                         Log.d("success", name["name"].toString())
 
-                        friendList.add(name["name"].toString())
+                        firestore.collection(currentUserEmail!!).document("Friend List")
+                                .get()
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        val document = task.result
+                                        if (document!!.exists()) {
+                                            firestore.collection(currentUserEmail).document("Friend List")
+                                                    .update("friend_name", FieldValue.arrayUnion(name["name"]))
+                                                    .addOnSuccessListener {
+                                                        toast("저장 성공")
+                                                        finish()
+                                                    }
+                                        }
+                                        else {
+                                            val friendList = arrayListOf<String>()    // 친구 이름 저장할 배열
 
-                        toast(friendList.toString())
+                                            friendList.add(name["name"].toString())
+                                            toast(friendList.toString())
+
+                                            val friendName = hashMapOf(
+                                                    "friend_name" to friendList)
+
+                                            firestore.collection(currentUserEmail).document("Friend List")
+                                                    .set(friendName)
+                                                    .addOnSuccessListener {
+                                                        toast("저장 성공")
+                                                        finish()
+                                                    }
+                                                    .addOnFailureListener {
+                                                        toast("저장 실패")
+                                                    }
+                                        }
+                                    }
+                                }
+                                .addOnFailureListener {
+                                    toast("fail")
+                                }
                     }
                 }
-
                     //실패하면
                 .addOnFailureListener { exception ->
                     Log.d("fail", exception.toString())
                     toast("사용자 정보 없음")
                 }
-
-            // 가져온 친구 이름 DB에 저장하기 위해 현재 사용자 아이디 찾기
-            // 현재 사용자 아이디를 식별하는 걸로 사용
-            // friendList - 현재 사용자 아이디 - 친구 이름(배열)
-            firestore.collection("users")
-                    .document(currentUser!!)
-                    .get()
-                    .addOnSuccessListener {
-                        val currentUserId = it.get("id").toString() // 현재 사용자 id 가져옴
-
-                        // DB에 저장하기 위한 key(friend_name), value(친구 이름 배열)
-                        val friendName = hashMapOf(
-                                "friend_name" to friendList
-                        )
-
-                        // DB에 저장 friendList - 현재 사용자 아이디 - 친구 이름
-                        firestore.collection("friendList").document(currentUserId)
-                                .set(friendName)
-                                .addOnSuccessListener {
-                                    toast("저장 성공")
-                                    finish()
-                                }
-                                .addOnFailureListener {
-                                    toast("저장 실패")
-                                }
-                    }
         }
     }
 }
